51772c4028229bf19f16864eee515f9982a8784a
Kyle Annen <kannen-contractor@cars.com>
2021-03-29 14:09:38 -0500

    feat: add tmate.conf

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ ln -s ${dir}/.bashrc ~/.bashrc
 ln -s ${dir}/.bash_profile ~/.bash_profile
 ln -s ${dir}/.vimrc ~/.vimrc
 ln -s ${dir}/.tmux.conf ~/.tmux.conf
+ln -s ${dir}/.tmate.conf ~/.tmate.conf
 ln -s ${dir}/fish ~/.config/fish
